1bc243e4957b669272da6f9c8eca7bb3b5ad42a5
Raymond <raymondleons1997@gmail.com>
2026-06-04 15:17:38 +0700

    Initial Commit

---
+++ PeekSy-Games/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PeekSy-Games
+//
+//  Created by Raymond Leon Sembiring on 04/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PeekSy-Games/PeekSy_GamesApp.swift
@@ -0,0 +1,17 @@
+//
+//  PeekSy_GamesApp.swift
+//  PeekSy-Games
+//
+//  Created by Raymond Leon Sembiring on 04/06/26.
+//
+
+import SwiftUI
+
+@main
+struct PeekSy_GamesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
